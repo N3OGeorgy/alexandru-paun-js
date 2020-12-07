03ea4f63dedf52a28bd752bb8ceebd9ebdf8de98
N3OGeorgy <HomeDesktop+N3OGeorgy@users.noreply.github.com>
2020-12-07 23:12:02 +0200

    tema 7 dec, partial

--- M02S03/ex04/app.js
+++ M02S03/ex04/app.js
@@ -1,8 +1,32 @@
 const img = document.querySelector('.image');
 const paragraph = img.parentElement;
 
+const messageContainer = document.createElement('span');
+messageContainer.innerText = 'imaginea se incarca';
+paragraph.append(messageContainer);
+
 img.addEventListener('load', () => {
-  const messageContainer = document.createElement('span');
-  messageContainer.innerText = 'imaginea s-a incarcat';
-  paragraph.append(messageContainer);
+  setTimeout(() => {
+    messageContainer.innerText = 'imaginea s-a incarcat';
+  }, 1000);
+  img.addEventListener('click', () => {
+    alert(`Imaginea cu URL-ul: ${img.src} s-a incarcat`);
+  });
 });
+
+const $img2 = $('.image2');
+const $paragraph2 = $img2.parent();
+
+const $messageContainer2 = $('<div>', {
+  text: 'imaginea se incarca2',
+});
+$paragraph2.append($messageContainer2);
+$img2
+  .on('load', () => {
+    setTimeout(() => {
+      $messageContainer2.text('imaginea s-a incarcat');
+    }, 1000);
+  })
+  .on('click', () => {
+    alert(`Imaginea cu URL-ul: ${img.src} s-a incarcat`);
+  });
--- M02S03/ex05/app.js
+++ M02S03/ex05/app.js
@@ -1,7 +1,38 @@
 const p = document.getElementById('dimensions');
+const p2 = document.getElementById('dimensions2');
+
+function showMe() {
+  let width = window.innerWidth;
+  let height = window.innerHeight;
+  p2.innerText = `Suprafata este de ${width * height} pixeli.`;
+}
+
 window.addEventListener('resize', () => {
   let width = window.innerWidth;
   let height = window.innerHeight;
-  p.innerText = width + '+' + height;
+  p.innerText = `Fereastra are acum suprafata de ${width * height} pixeli.`;
   // alert('fereastra si-a schimbat dimensiunea');
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  showMe();
+});
+
+const $pjs = $('#dimensionsjs');
+const $pjs2 = $('#dimensionsjs2');
+
+function showMe2() {
+  let width = $(window).width();
+  let height = $(window).height();
+  $pjs2.text(`Suprafata este de ${width * height} pixeli.`);
+}
+
+$().ready(function () {
+  showMe2();
+});
+
+$(window).resize(function () {
+  let width = $(window).width();
+  let height = $(window).height();
+  $pjs.text(`Fereastra are acum suprafata de ${width * height} pixeli.`);
 });
